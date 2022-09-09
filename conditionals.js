37a9ecc7cb70a21337fbb0ceda267d0a3ff48a3a
David Wang <18davidwang@gmail.com>
2022-09-08 19:22:04 -0700

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -23,3 +23,7 @@ const temperature = 30;
 if (temperature < -40 || temperature > 40) {
   console.log("Maybe going outside isn't such a great idea…");
 }
+
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
